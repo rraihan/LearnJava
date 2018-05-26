37e4730b143e9d828a2c68ebbefaa37bc0150558
rraihan <dialraihan@gmail.com>
2018-05-26 17:06:48 -0400

    First Commit

---
+++ src/project1/Exercise1TextFormat.java
@@ -0,0 +1,20 @@
+package project1;
+
+//TODO Auto-generated method stub
+		/* Author: Rakib Raihan
+		 * Date: May 26,2018
+		 * 
+		 */
+public class Exercise1TextFormat {
+	
+
+			public static void main(String[] args) {
+				System.out.println("Deshi Halal Resturant");
+				System.out.println("Hours:\t Mon\t Fri,\t9am-9pm\n");
+				System.out.println("\tLowest Price In Jamaica");
+				System.out.print("\tLunch\tDiner\n");
+				System.out.println("\t$5.95\t$11.95");
+
+	}
+
+}
---
+++ src/project1/Exercise2IntVariables.java
@@ -0,0 +1,24 @@
+package project1;
+
+
+/*int 4 bytes,
+ * 
+ *Variable : Memory Location
+ * Integer :whole number. From -2billion to +2billion
+ * 
+ */
+
+public class Exercise2IntVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age1, age2, age3;
+		
+		age1=10;
+		age2=14;
+		age3=36;
+		System.out.println(age1+ "," +age2 + "," +age3);
+		
+	}
+
+}
