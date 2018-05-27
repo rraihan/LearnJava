de67619a478b02786b7689573a359972adddee7e
rraihan <dialraihan@gmail.com>
2018-05-26 21:46:26 -0400

    second commit on May 26 @9:50 pm.

---
+++ src/project1/Exercise3IntMath.java
@@ -0,0 +1,27 @@
+package project1;
+/*
+ 
+	A sailboat sail is triangular and has height of 14 ft. 
+	The length base of the flag is 6 ft. 
+	If area =base*height/2
+	Find the area of the sail.
+
+ */
+
+public class Exercise3IntMath {
+
+	public static void main(String[] args) {
+
+		int height,base,area;
+		height = 14;
+		base = 6;
+		area = base*height/2;
+		
+		System.out.println("The area of the sail is "+ area + " squre feet.");
+		
+		
+		
+
+	}
+
+}
---
+++ src/project1/Exercise4FlotDouble.java
@@ -0,0 +1,15 @@
+package project1;
+
+
+public class Exercise4FlotDouble {
+
+	public static void main(String[] args) {
+		// convert 10.5 feet to meters. There are 0.305 meters per feet.
+		double  feet, meters;
+		feet = 10.5;
+		meters = feet*0.305;
+  
+		System.out.println(feet + " feet is equial to " +meters +" meters.");
+	}
+
+}
